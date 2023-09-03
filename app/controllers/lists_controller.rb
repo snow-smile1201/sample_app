af50e7bc5bb0bbbbcb8e9607020782cc67397253
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-03 03:21:32 +0000

    [4-4]学習終了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,20 +3,22 @@ class ListsController < ApplicationController
     #viewへ渡すためのインスタンス変数に空のmodelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     #1&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
     #3 データをデータベースに保存するSAVEメソッドの実行
     list.save
-    #4 トップ画面にリダイレクト
-    redirect_to '/top'
+    #4 Show画面にリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
